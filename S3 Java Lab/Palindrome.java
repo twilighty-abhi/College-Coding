568bc2b593f12591c4c084c0f3ec8c639c54ea52
twilighty-abhi <abhiramnj@gmail.com>
2023-02-17 09:56:18 +0530

    palindrome update

---
+++ S3 Java Lab/Palindrome.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Palindrome{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int flag = 0;
+        String s = sc.nextLine();
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i) == s.charAt(s.length()-1-i)){
+                flag = 1;
+            }
+        }
+        if(flag==0){
+            System.out.println("not a palindrom");
+        }
+        else{
+            System.out.println("it is a palindrom");
+        }
+        sc.close();
+    }
+}
