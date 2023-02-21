0a9f3ab482ab6e64899fd8fe424cd692b3bf052d
Abhiram N J <abhiramnj@gmail.com>
2023-02-21 17:56:59 +0530

    update

--- S3 Java Lab/Palindrome.java
+++ S3 Java Lab/Palindrome.java
@@ -1,20 +1,26 @@
+import java.io.*;
 import java.util.*;
-class Palindrome{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int flag = 0;
-        String s = sc.nextLine();
-        for(int i=0;i<s.length()/2;i++){
-            if(s.charAt(i) == s.charAt(s.length()-1-i)){
-                flag = 1;
-            }
-        }
-        if(flag==0){
-            System.out.println("not a palindrom");
-        }
-        else{
-            System.out.println("it is a palindrom");
-        }
-        sc.close();
+
+class Palindrome
+{
+  public static void main (String args [])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the String to check for palindrome: ");
+    String s=sc.nextLine();
+    int flag=0;
+    for (int i=0;i<s.length()/2 ;i++ )
+    {
+      if(s.charAt(i) == s.charAt(s.length()-1-i))
+      {
+        flag = 1;
+        break;
+      }
     }
+    if(flag==0)
+    System.out.println("Is NOt a palindrome");
+    else
+    System.out.println("Is a palindrome");
+
+  }
 }
