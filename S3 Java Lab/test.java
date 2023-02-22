18f0bca20e99776a6936a48be3c25bfda047e6b9
Abhiram N J <abhiramnj@gmail.com>
2023-02-22 07:51:46 +0530

    update

--- S3 Java Lab/test.java
+++ S3 Java Lab/test.java
@@ -1,11 +1,20 @@
-import java.io.*;
+import java.util.*;
 
-class test{
+public class GFG
+{
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("enter no. of rows of matrix 1");
+        int a = sc.nextInt();
+       int[] ary = new int[a];
+       for(int i=0; i<a; i++)
+                ary[i] = sc.nextInt();
 
-  public static void main(String[] args) {
-    int a=10;
-    float b=34.87f;
-    int c=(int)b;
-    System.out.print("Hello world!\n"+ b +"\t" + c);
-  }
+      System.out.println("Inputted array is:"+ Arrays.toString(ary));
+      //  int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 };
+        Arrays.sort(ary);
+
+        System.out.println("Modified arr[] : " + Arrays.toString(ary));
+    }
 }
